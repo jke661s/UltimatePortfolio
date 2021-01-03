d9878ba9988f42967f9bd3a0f44f9b5812f9f85c
Jackie <jke661s@gmail.com>
2021-01-04 04:04:07 +1100

    Cleaned up Core Data

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -50,7 +50,7 @@ class DataController: ObservableObject {
             for j in 1...10 {
                 let item = Item(context: viewContext)
                 item.title = "Item \(j)"
-                item.creationData = Date()
+                item.creationDate = Date()
                 item.completed = Bool.random()
                 item.project = project
                 item.priority = Int16.random(in: 1...3)
