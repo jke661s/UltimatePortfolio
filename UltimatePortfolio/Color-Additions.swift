66e034a51d6a798770d6095f70c218e65bc2864f
Jackie <jke661s@gmail.com>
2021-01-26 08:22:08 +1100

    Created the Home view

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Jackie Wang on 25/1/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
